3948228357a8342ae7e2f62bd1a5964da2c9b476
Dana Dorn <dorndana8899@gmail.com>
2025-06-13 09:26:55 +0700

    add: promt

---
+++ promt.js
@@ -0,0 +1,6 @@
+let A = prompt("Enter the value A:");
+let B = prompt("Enter the value B:");
+//parseInt : cover str to int
+let result = parseInt (A) +parseInt(B);
+
+console.log("Result:", result);
