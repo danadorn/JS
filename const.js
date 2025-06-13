7e908e71dac1cb90338a1b04b7cb7ad5990e02f5
Dana Dorn <dorndana8899@gmail.com>
2025-06-13 09:49:41 +0700

    add: const

---
+++ const.js
@@ -0,0 +1,4 @@
+// using const keyword
+const firstNuumber = 100; // initialize value to firstNumber
+firstNuumber = 300; // runt time error: cus const cannot assign value agian after initialized
+console.log(firstNuumber)
