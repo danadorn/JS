6a20dee01e77155245e90c6fa49a38df37e368d4
Dana Dorn <dorndana8899@gmail.com>
2025-06-16 10:03:34 +0700

    add: DataType/boolean

---
+++ DataType/boolean.js
@@ -0,0 +1,9 @@
+const isFalse = true;
+// mostly using variable name declare with boolean using is as prefix
+const isChecked = false;
+let isConfirm = confirm("Do you like this class session?")
+if(isConfirm == true){
+    console.log("Yes, I do.")
+} else{
+    console.log("No, I dont.")
+}
