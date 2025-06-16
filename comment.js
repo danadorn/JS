92d2feea02fad15e2dbeb2d481de1d9763d084a5
Dana Dorn <dorndana8899@gmail.com>
2025-06-16 08:46:56 +0700

    add: 3types of comments

---
+++ comment.js
@@ -0,0 +1,18 @@
+// single comment
+const firstName = "Kon Khmer";
+
+// multi-line comment
+/* ehhh
+yes
+*/
+
+
+// documentation comment
+/**
+ * @param {number} a - A is a number
+ * @param {number} b - B is a number
+ * @return {number}
+ */
+function substract(a,b) {
+    return a - b;
+}
