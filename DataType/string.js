efdfb6526d62028aebcba397b44e5795a5e9629a
Dana Dorn <dorndana8899@gmail.com>
2025-06-16 09:36:20 +0700

    add:DataType/string

---
+++ DataType/string.js
@@ -0,0 +1,5 @@
+const firstName = "Kon"
+const lastName = "Khmer"
+const result = `My first name is ${firstName}, and my last name is ${lastName}`
+console.log(result);
+console.log(`Type of result ${typeof(result)}`)
