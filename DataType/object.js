0fc89e841ae1317851406eb2624632bd488a6870
Dana Dorn <dorndana8899@gmail.com>
2025-06-17 08:45:49 +0700

    add: DataType/object

---
+++ DataType/object.js
@@ -0,0 +1,21 @@
+// const Students = {
+//     name: 'Christ Evans',
+//     gender: 'Female',
+//     age: 42,
+//      isCame: true
+// };
+// Students.gender = 'Male';
+// // display as a table
+// console.table([Students])
+
+const Employee = {
+    id: 101,
+    salary: 10,
+    position: 'Blockchain Developer'
+};
+const Person = {
+    dateOfBirth: '02/11/2001',
+    placeOfBirth: 'Battambang'
+};
+console.table([Employee]);
+console.table([Person]);
